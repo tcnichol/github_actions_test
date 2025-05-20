a8876471f6281184ab57803865015fb0c0435a3e
toddn <tcnichol@illinois.edu>
2025-05-20 16:48:38 -0500

    changing the version

--- second_test.py
+++ second_test.py
@@ -1,9 +1,11 @@
 import time
 
+version="v1.0.6"
 
 def main():
     num_loops = 0
     while (True):
+        print(f"Version is {version}")
         print("Testing testing")
         print(f"Loop number {num_loops}")
         print("waiting ten seconds...")
